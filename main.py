3c74551eacb543eb957d127f61995d09d1afb407
Jose M. Esnaola <josemesnaola@gmail.com>
2016-09-22 15:37:41 +0200

    New method to parse options. Now the configuration file is necessary, not optional. A little bit more complex, but much more easy to expand. More general. Basic things:     - Added group support.     - Conf file handled by Yaml.     - Parsing wit argparse.     TODO: save parameters in a config file. (last used)

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import sys
 
 __author__ = 'jm'
 
+
 # Empty class to manage external parameters
+# noinspection PyClassHasNoInit
 class Options:
     pass
+
 
 options = None
 ops = Options()
@@ -27,22 +30,22 @@ except yaml.YAMLError, exc:
     print "Error in configuration file:", exc
     exit(-1)
 
-gopts = options[0]
-
 # We load parameters from the dictionary of the conf file and add command line options (2nd parsing)
 parser = argparse.ArgumentParser(
     description='Simulator of an all-to-all network of QIF neurons with synaptic depression dynamics.',
     usage='python %s [-O <options>]' % sys.argv[0])
 
-for key in gopts.keys():
-    flags = key.split()
-    parser.add_argument(*flags, default=gopts[key]['default'], help=gopts[key]['description'], dest=flags[0][1:],
-                        metavar=gopts[key]['name'], type=type(gopts[key]['default']),
-                        choices=gopts[key]['choices'])
+for group in options:
+    gr = parser.add_argument_group(group)
+    for key in options[group]:
+        flags = key.split()
+        opts = options[group]
+        gr.add_argument(*flags, default=opts[key]['default'], help=opts[key]['description'], dest=flags[0][1:],
+                            metavar=opts[key]['name'], type=type(opts[key]['default']),
+                            choices=opts[key]['choices'])
 
 # We parse command line arguments:
 args = parser.parse_args(farg[1], namespace=ops)
 
 extopts = {"dt": 1E-3, "t0": 0.0, "ftau": 20.0E-3, "modes": [10, 7.5, -2.5]}
 pertopts = {"dt": 0.5, "attack": 'exponential', "release": 'instantaneous'}
-
